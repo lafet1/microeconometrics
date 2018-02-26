05b3560c86ef750c09981f7195d3a27a6db18133
lafet1 <32439234+lafet1@users.noreply.github.com>
2018-02-26 13:20:50 +0100

    working with the final data set
    some problems with multicollinearity present, not fully working yet

--- ass1/code_ass1.R
+++ ass1/code_ass1.R
@@ -1,17 +1,26 @@
 # packages
 library(maxLik)
 library(dplyr)
+library(readstata13)
 library(zeligverse)
+library(dummies)
 
 
 # data load
-data <- read.csv('data_ass1.csv')
-x <- data[, c('FAMSZEYR', 'ENDRFD12', 'RURSLT42', 'AGE31X')]
-# x <- x %>% mutate(cyl = factor(cyl, levels = c(4, 6, 8)))
-x <- as.matrix(x)
-y <- data[, 'OBDRV12']
-k <- ncol(x)
-n <- nrow(x)
+data <- read.dta13('clean_data_micro.dta')
+x <- data[, c('adsmok42', 'age12x', 'ttlp12x', 'sex', 'racewx', 'eduyrdeg',
+              'inscov12', 'marry12x', 'famsze12', 'region12', 'rthlth42', 'adinsb42',
+              'adover42', 'phyexe53')]
+x <- x %>% mutate_if(is.integer, as.numeric) 
+s <- x %>% mutate_if(is.character, as.factor)
+whatever <- cumsum(sapply(s %>% select_if(is.factor), function(x) length(unique(x))))
+dummies <- s %>% select_if(is.factor) %>% dummy.data.frame
+dummies <- dummies[, -whatever]
+X <- bind_cols(x %>% select_if(is.numeric), dummies)
+X <- as.matrix(X)
+y <- data[, 'obdrv12']
+k <- ncol(X)
+n <- nrow(X)
 
 # theta is both lambda and beta coefficients
 beta <- rep(0, k)
@@ -42,12 +51,12 @@ mlf <- function(param){
   bet <- param[1:k]
   mu <- param[(k + 1):(k + 7)]
   # we get the Nx1 matrix of fitted values and prepare an empty vector for probabilities
-  xb <- x %*% bet
+  xb <- X %*% bet
   p0 <- rep(0, length(y))
   
   # this for loop calculates the probability per observation
   for (j in 0:6){
-    
+  
     # firstly we obtain the probability for 0 visits
     if (j == 0){
       p0 <- p0 + plogis(mu[1] - xb) * recoder(y, j)
@@ -76,12 +85,16 @@ mlf <- function(param){
 mles <- maxLik(mlf, grad = NULL, hess = NULL, start = c(beta, mu), method = "bfgs",
               iterlim = 100)
 
+
 ###### Estimation using zelig()
 
-data <- as.data.frame(cbind(as.factor(ifelse(y >= 6, 6, y)) , x))
-colnames(data) <- c('y', 'FAMSZEYR', 'ENDRFD12', 'RURSLT42', 'AGE31X')
-ologit <- zelig(y ~ FAMSZEYR + ENDRFD12 + RURSLT42 + AGE31X, model = "ologit", data = data)
-
+mydata <- as.data.frame(cbind(as.factor(ifelse(y >= 6, 6, y)) , x))
+mydata <- mydata %>% mutate_if(is.character, as.factor) 
+colnames(mydata) <- c('y', colnames(x))
+ologit <- polr(y ~ adsmok42 + age12x + ttlp12x + sex + racewx + eduyrdeg +
+                inscov12 + marry12x + famsze12 + region12 + rthlth42 + adinsb42 +
+                adover42 + phyexe53, method = 'logistic', data = mydata)
+ologit
 
 
 
