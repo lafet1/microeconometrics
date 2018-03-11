6b809454093582a4e02a21696ed4f2b32219dfda
lafet1 <32439234+lafet1@users.noreply.github.com>
2018-03-11 08:22:07 +0100

    bootstrap results, code comments

--- ass2/code_ass2.R
+++ ass2/code_ass2.R
@@ -6,7 +6,7 @@ library(dplyr)
 ##### data load #####
 data <- read.csv('workdata.csv')
 
-# intermediate data
+# data preparation
 Z <- select(data, female, age, age2, highschool, bachelor, master, 
             doctorate, married, midwest, south, west, white, black, native, 
             asian, disability, business, hand_labour, retail, educ_health_social, 
@@ -26,27 +26,38 @@ X <- select(data, female, age, age2, highschool, education, fam_size,
 Y1 <- as.integer(data$insured)
 Y2 <- data$expenditure
 
-# 'estimation'
+
+###### Estimation ######
+
+# we start by estimating the model without bootstrap to get and idea
 start <- Sys.time()
 bw <- npindexbw(xdat = Z, ydat = Y1,
                 nmulti = 5, optim.reltol = 0.1)
 end <- start - Sys.time()
-# 5 variable - 9.5 min, 6 variables - 58 mins, 7 variables - 54 mins, 
-# 8 variables - 1.05 hours
-# 
 
+# the nonparametric approach is very slow so we impose tolerance in the optimization
+# routine and get less precise results, still the estimation takes more than 10 minutes
+
+# now we simply estimate the nonparametric probit and get the fitted values
 np_probit <- npindex(bws = bw)
 fit <- fitted(np_probit)
+
+# data frame we need for second equation
 model_data <- data.frame(Y2, X, as.factor(Y1), fit, fit^2, fit^3)
 
+# second equation using the Newey estimator
 main_model <- lm(Y2 ~ ., data = model_data)
 
 
+####### Bootstrap ######
+
 boot_se <- function(data, index = nrow(data)){
+  # function for bootstrapping the SE's
   
+  # bootstrap sample
   data <- data[sample(nrow(data), size = index, replace = T), ]
   
-  # first we get variables to resample 
+  # we get the variables from the bootstrap sample
   Z <- select(data, female, age, age2, highschool, bachelor, master, 
               doctorate, married, midwest, south, west, white, black, native, 
               asian, disability, business, hand_labour, retail, educ_health_social, 
@@ -66,24 +77,30 @@ boot_se <- function(data, index = nrow(data)){
   Y1 <- as.integer(data$insured)
   Y2 <- data$expenditure
 
-  # second we run the nonparametric probit
+  # we run the nonparametric probit
   bw <- npindexbw(xdat = Z, ydat = Y1, 
                   nmulti = 5, optim.reltol = 0.1)
   np_probit <- npindex(bws = bw)
   fit <- fitted(np_probit)
+  
+  # prepare the data for second equation
   model_data <- data.frame(Y2, X, Y1 = as.factor(Y1), fit, fit^2, fit^3)
   
+  # estimate second equation
   main_model <- lm(Y2 ~ ., data = model_data)
   
+  # return the model summary so we can get the bootstrapped SE's
   return(coef(summary(main_model)))
   
 }
 
-
+# for loop for estimating the actual bootstrap replication and preparation of the list
 results <- list()
-
 for (i in 1:50) {
   results[[i]] <- boot_se(data)
 }
 
+# saveRDS(results, file = 'bootstrap_results.rds')
+# to load results use readRDS('bootstrap_results.rds') - RDS allows us to save and read
+# list objects, this loop took the night, so it is good practice to save it
 
